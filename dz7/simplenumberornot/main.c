407cf773c6f55eb1d8cb05d628b13c4e2a17a5ad
Somaxa <Somaxa>
2014-06-30 16:35:20 +0300

    dz7

--- dz7/simplenumberornot/main.c
+++ dz7/simplenumberornot/main.c
@@ -3,18 +3,26 @@
 
 int main()
 {
-    int enteringNumber, counter1;
+    int enteringNumber, counter1, likeBooleanVariable;
     printf("Hello user! Let's check your number.\n");
     printf("Please, enter the number: ");
     scanf("%d", &enteringNumber);
-    for(counter1=enteringNumber-1;counter1>0;--counter1)
+    for(counter1=enteringNumber-1;counter1>1;--counter1)
     {
-        if(enteringNumber%counter1!=0)
-        printf("Your number is not simple.");
+        if(enteringNumber%counter1==0)
+        {
+            likeBooleanVariable=1;
+            break;
+        }
+    }
+    if(likeBooleanVariable==1)
+    {
+        printf("Your number is simple");
     }
     else
     {
-        printf("Your number is simple.");
+        printf("Your number is not simple");
     }
+
     return 0;
 }
