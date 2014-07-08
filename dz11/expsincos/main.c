4ba6844b0e7f24b10efd19ab9b6a797b0d2fbafa
Somaxa <somaxa@mail.ru>
2014-07-08 14:33:02 +0300

    dz11

--- dz11/expsincos/main.c
+++ dz11/expsincos/main.c
@@ -4,6 +4,17 @@
 
 int main()
 {
+<<<<<<< HEAD
+    double exponentour, exponentmath, x=1.0, counter1;
+    printf("Hello user!\n");
+    exponentmath=exp(x);
+    for(counter1=-2.0;counter1<=2.0;counter1=+0.1)
+        exponentour=exponentour;
+
+
+    printf("x    exp(our)    exp(math)    exp(m-o)\n");
+    printf("%d       %f            %f            %f", x, exponentour, exponentmath,exponentmath-exponentour);
+=======
     double x, exponent=1.0, factorial=1.0, epsilon=0.001;
     printf("Hello user!\n");
     for(x=-2.0;x<2.0;x=x+0.1)
@@ -16,5 +27,6 @@ int main()
        printf("%d\n", x);
 
     }
+>>>>>>> d6d1545fbf43b3f7a39086a5b7938c99a4318a14
     return 0;
 }
