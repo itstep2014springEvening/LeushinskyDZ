f4ef5eda0dbbc25b694285ad0447afc532a26255
Somaxa <somaxa@mail.ru>
2014-07-21 17:03:33 +0300

    dz13

--- dz13/xdegpwithfunc/main.c
+++ dz13/xdegpwithfunc/main.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+double degreeder(double X, int P);
 int main()
 {
-    int degreeder(int X, int P);
-    int X, P, degreeResult=1;
+    double X;
+    int P;
     printf("Hello user!\n");
     printf("Please, enter X: ");
-    scanf("%d", &X);
+    scanf("%lf", &X);
     printf("Please, enter P: ");
     scanf("%d", &P);
-    degreeResult=degreeder(X, P);
-    P>0?printf("Result = %d.", degreeResult):printf("Result = 1/%d.", degreeResult);
+    printf("Result = %.5f.", degreeder(X, P));
     return 0;
 }
 
-int degreeder(int X, int P)
+double degreeder(double X, int P)
 {
-    int degreeResult=1;
+    double degreeResult=1.0;
     for(int counter1=0; counter1<abs(P); ++counter1)
     {
         degreeResult=degreeResult*X;
-        printf("%d\n",degreeResult);
+    }
+    if(P<0)
+    {
+        degreeResult=1/degreeResult;
     }
     return degreeResult;
 }
