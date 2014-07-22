d3010fd813dbf8984c8686a36b84990141c01ae8
Somaxa <somaxa@mail.ru>
2014-07-22 17:22:45 +0300

    dz15

---
+++ dz15/maxminrandarray/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int size=10, ourArray [size], usingMassiveLength, max=1, min=1;
+    printf("123!\n");
+    scanf("%d", &usingMassiveLength);
+    ourArray [usingMassiveLength];
+    for(int counter1=0;counter1<usingMassiveLength;++counter1)
+    {
+        ourArray[counter1]=rand();
+    }
+    min=ourArray[1];
+    for(int counter2=0;counter2<usingMassiveLength;++counter2)
+    {
+        if(max<ourArray[counter2])
+        {
+            max=ourArray[counter2];
+        }
+        if(min>ourArray[counter2])
+        {
+            min=ourArray[counter2];
+        }
+        printf("%d\n",ourArray[counter2]);
+    }
+    printf("Max = %d\n", max);
+    printf("Min = %d", min);
+    return 0;
+}
