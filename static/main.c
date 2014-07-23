1e15ec76ac949fe732569e3e7453bc083d0ed594
Somaxa <somaxa@mail.ru>
2014-07-23 18:42:39 +0300

    static

---
+++ static/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c;
+    printf("Hello world!\n");
+    for(c=1;c<=50;++c)
+    {
+        zu();
+    }
+    return 0;
+}
+
+
+void zu()
+{
+    int n=0;
+    printf("%d\n",n);
+    ++n;
+}
