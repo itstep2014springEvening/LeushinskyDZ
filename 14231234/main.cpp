cc03528ee105a30bf10b9ee641c62c35c382a329
Somaxa <somaxa@mail.ru>
2014-07-24 18:13:01 +0300

    14231234

---
+++ 14231234/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+
+    printf("a = ");
+    scanf("%d", &a);
+    int b=a*152-75;
+    printf("b = %d", b);
+
+    return 0;
+}
---
+++ 14231234/main.h
@@ -0,0 +1,3 @@
+int summa(int a, int b)
+summa=a+b;
+return 0;
