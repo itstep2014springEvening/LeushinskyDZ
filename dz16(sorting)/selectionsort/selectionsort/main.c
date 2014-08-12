49cb4c4a63713b057bd3c781cb278378cb1b3144
Somaxa <somaxa@mail.ru>
2014-08-12 15:12:47 +0300

    dz16(sorting)

--- dz16(sorting)/selectionsort/selectionsort/main.c
+++ dz16(sorting)/selectionsort/selectionsort/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../../generelfunctions/input.h"
-#include "../../generelfunctions/output.h"
+#include "../../generelfunctions/generelfunctions.h"
 
 int main()
 {
