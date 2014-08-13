cc8a9f2a763ef21eded4661f34e701692a933222
Somaxa <Somaxa@mail.ru>
2014-08-13 15:15:20 +0200

    dz16(sorting)

--- dz16(sorting)/selectionsort/main.c
+++ dz16(sorting)/selectionsort/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../generelfunctions/generelfunctions.h"
 
+int main()
+{
+    int size=5;
+    int ourArray [arrayMaxSize];
+    int temporaryVariable=0, max=0;
+    input (ourArray, size);
+    for(int counter=0; counter<size; ++counter)
+    {
+        if(ourArray[max]<ourArray[counter])
+        {
+            temporaryVariable=ourArray[counter];
+            ourArray[counter]=ourArray[max];
+            ourArray[max]=temporaryVariable;
+        }
+        max++;
+    }
+    output (ourArray, size);
+    return 0;
+}
