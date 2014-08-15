255aaa4cf513f54bb97ce772c7342ea744213eca
Somaxa <Somaxa@mail.ru>
2014-08-15 15:15:37 +0200

    dz16(sorting)

--- dz16(sorting)/selectionsort/main.c
+++ dz16(sorting)/selectionsort/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../generelfunctions/generelfunctions.h"
+int maxIndex (int ourArray[], int size);
+void changeMax (int ourArray[], int temporaryVariable1, int temporaryVariable2);
 
 int main()
 {
@@ -8,16 +10,31 @@ int main()
     int ourArray [arrayMaxSize];
     int temporaryVariable=0, max=0;
     input (ourArray, size);
-    for(int counter=0; counter<size; ++counter)
+    for (int i=0; i<size; ++i)
     {
-        if(ourArray[max]<ourArray[counter])
-        {
-            temporaryVariable=ourArray[counter];
-            ourArray[counter]=ourArray[max];
-            ourArray[max]=temporaryVariable;
-        }
-        max++;
+        int index=maxIndex(ourArray, size-i);
+        changeMax (ourArray, size-i-1, index);
     }
     output (ourArray, size);
     return 0;
 }
+
+int maxIndex (int ourArray[], int size)
+{
+    int max=0;
+    for(int counter=0; counter<size; ++counter)
+    {
+        if(ourArray[max]<ourArray[counter])
+            max=counter;
+    }
+    return max;
+}
+
+void changeMax (int ourArray[], int temporaryVariable1, int temporaryVariable2)
+{
+    int varForChange=ourArray[temporaryVariable1];
+    ourArray[temporaryVariable1]=ourArray[temporaryVariable2];
+    ourArray[temporaryVariable2]=varForChange;
+}
+
+
