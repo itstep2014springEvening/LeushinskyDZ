f4ab7bfa8d672f77ecc6dd5b00fa8ee8b6eb2953
Somaxa <somaxa@mail.ru>
2014-08-18 20:34:52 +0300

    RPN

---
+++ RPN/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("!REVERSE POLISH NOTATION!");
+    int stack [256];
+    int top=-1,value;
+    //stack[++top]=value;//push
+    //value=stack[top--];//pop
+    //value=stack[top];//onTop
+    //top<0;//isEmpty
+    //top=-1;//clear
+
+
+    return 0;
+}
