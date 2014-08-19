bb157908822240f3d36f310390d2ce4ace250b9d
Somaxa <somaxa@mail.ru>
2014-08-19 16:50:16 +0300

    insertionsort

--- dz16(sorting)/insertionsort/main.c
+++ dz16(sorting)/insertionsort/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../generelfunctions/generelfunctions.h"
+void insertionsort (int ourArray[], int size);
 
 int main()
 {
     int size=5;
     int ourArray [arrayMaxSize];
     input (ourArray, size);
-    insertsort(ourArray, size);
+    insertionsort(ourArray, size);
     output (ourArray, size);
     return 0;
 }
 
-void insertsort (int ourArray[], int size)
+void insertionsort (int ourArray[], int size)
 {
     int firstElement, secondElement, temporaryVariable;
     for(int counter1=1;counter1<size;++counter1)
