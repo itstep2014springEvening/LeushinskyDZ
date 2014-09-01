ef09fb5608dd5601506d16ecfd3c83c1e0469853
Somaxa <somaxa@mail.ru>
2014-09-01 13:25:05 +0300

    dz17

---
+++ dz17/RPN/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("!REVERSE POLISH NOTATION!");
+    double stack [256];
+    char currentValue;
+    int top=-1, temporaryVariableOne=0, temporaryVariableTwo=0;
+    do
+    {
+        scanf("%c", &currentValue);
+        if(currentValue>='0' && currentValue<='9')
+        {
+            stack[++top]=(double)(currentValue-48);
+
+        }
+        if(currentValue=='+')
+        {
+            ++top;
+            temporaryVariableTwo=stack[--top];
+            temporaryVariableOne=stack[--top];
+            stack[top]=temporaryVariableTwo+temporaryVariableOne;
+        }
+        if(currentValue=='=')
+        {
+            break;
+        }
+    }
+    while(currentValue != '=');
+    printf("%f", stack[top]);
+    return 0;
+}
+
+
