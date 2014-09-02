8b3fd7ad59303ec9741e8be9fbe249bda8e8a9ac
Somaxa <somaxa@mail.ru>
2014-09-02 17:19:02 +0300

    dz17

---
+++ dz17/BFFullVersion/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arrayMaxSize=256;
+    char ourArray[arrayMaxSize];
+    int top=0;
+    char head;
+    do
+    {
+        scanf("%c", &head);
+        switch (head)
+        {
+        case '+':
+            ++ourArray[top];
+            break;
+        case '-':
+            --ourArray[top];
+            break;
+        case '>':
+            ++top;
+            break;
+        case '<':
+            --top;
+            break;
+        case '.':
+            printf("%c", ourArray[top],top);
+            break;
+        case ',':
+
+            break;
+        case '[':
+
+            break;
+        case ']':
+
+            break;
+        }
+    }
+    while(head!=';');
+
+    return 0;
+}
