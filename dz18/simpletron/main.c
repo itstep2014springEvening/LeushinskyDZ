84cbbf7ef8c6089f1e6de7d808369f920aa52207
Somaxa <somaxa@mail.ru>
2014-09-09 17:30:01 +0300

    dz18

--- dz18/simpletron/main.c
+++ dz18/simpletron/main.c
@@ -3,6 +3,79 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("Simpletron is ready, enter your code.\n");
+    int top=-1, adress=0, operation=0, instructions=0, read=0, battery=0, turn=0;
+    int memory[99];
+    while(read!=-99999)
+    {
+        printf("%2d ", turn);
+        scanf("%d", &read);
+        memory[turn]=read;
+        turn++;
+    }
+    printf("Your code was loaded.\n");
+    do
+    {
+        operation=memory[instructions]/100;
+        adress=memory[instructions]%100;
+        switch(operation)
+        {
+        case 10:
+            scanf("%d", &memory[adress]);
+            instructions++;
+            break;
+        case 11:
+            printf("%+05d\n", memory[adress]);
+            instructions++;
+            break;
+        case 20:
+            battery=memory[adress];
+            instructions++;
+            break;
+        case 21:
+            memory[adress]=battery;
+            instructions++;
+            break;
+        case 30:
+            battery+=memory[adress];
+            instructions++;
+            break;
+        case 31:
+            battery-=memory[adress];
+            instructions++;
+            break;
+        case 32:
+            battery/=memory[adress];
+            instructions++;
+            break;
+        case 33:
+            battery*=memory[adress];
+            instructions++;
+            break;
+        case 40:
+            instructions=adress;
+            break;
+        case 41:
+            if(battery<0)
+            {
+                instructions=adress;
+            }
+            else
+                instructions++;
+            break;
+        case 42:
+            if(battery==0)
+            {
+                instructions=adress;
+            }
+            else
+                instructions++;
+            break;
+        case 43:
+            printf("Program is finished.");
+            break;
+        }
+    }
+    while(operation!=43);
     return 0;
 }
