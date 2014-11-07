0b614b148aa43417235fdf8fd32df2351238e806
Somaxa <somaxa@mail.ru>
2014-11-07 13:34:46 +0300

    addMenu

--- exam/main.c
+++ exam/main.c
@@ -12,11 +12,77 @@ typedef struct _Mountain
 
 int main()
 {
+    int menuChooses;
     printf("Hello, user. It's mountains DB. What do you want?\n\n");
     printf("1. Create new DB.\n");
     printf("2. Add a new record.\n");
     printf("3. Delete record.\n");
     printf("4. Sort fields.\n");
     printf("5. Exit.\n");
-    return 0;
+    scanf("%d", &menuChooses);
+
+    switch (menuChooses)
+{
+case 1:
+
+    break;
+case 2:
+
+    break;
+case 3:
+
+    break;
+case 4:
+
+    break;
+case 5:
+
+    break;
+
+    default:
+
+    break;
 }
+
+return 0;
+}
+
+void DBLoader()
+{
+
+}
+
+void DBSaver()
+{
+
+}
+
+void DBCreation()
+{
+
+}
+
+void addRecord()
+{
+
+}
+
+void deleteRecord()
+{
+
+}
+
+void DBSort()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
