632039cfe90e5af3b459de609060ee89f5b76678
Somaxa <somaxa@mail.ru>
2014-11-09 17:40:18 +0300

    dbcreating

--- exam/main.c
+++ exam/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void DBCreator();
+
 typedef struct _Mountain
 {
     char mountainName [256];
@@ -13,44 +15,51 @@ typedef struct _Mountain
 int main()
 {
     int menuChooses;
-    printf("Hello, user. It's mountains DB. What do you want?\n\n");
-    printf("1. Create new DB.\n");
-    printf("2. Add a new record.\n");
-    printf("3. Edit record.\n");
-    printf("4. Delete record.\n");
-    printf("5. Sort fields.\n");
-    printf("6. Exit.\n");
-    scanf("%d", &menuChooses);
+    do
+    {
+
+        printf("Hello, user. It's mountains DB. What do you want?\n\n");
+        printf("1. Create new DB.\n");
+        printf("2. Add a new record.\n");
+        printf("3. Edit record.\n");
+        printf("4. Delete record.\n");
+        printf("5. Sort fields.\n");
+        printf("0. Exit.\n");
+        scanf("%d", &menuChooses);
 
 
-    switch (menuChooses)
-{
-case 1:
 
-    break;
-case 2:
 
-    break;
-case 3:
+        switch (menuChooses)
+        {
+        case 1:
+        DBCreator();
+            break;
+        case 2:
 
-    break;
-case 4:
+            break;
+        case 3:
 
-    break;
-case 5:
+            break;
+        case 4:
 
-    break;
-    case 6:
-    system("cls");
-    printf("Bye-bye, user.");
+            break;
+        case 5:
+
+            break;
+        case 0:
+            system("cls");
+            printf("Bye-bye, user.");
+            return 0;
+            break;
+        default:
+            system("cls");
+
+            printf("Error! Please, enter correct menu number, or 0 to exit.\n\n");
+            break;
+        }
+    }while(menuChooses!=0);
     return 0;
-    break;
-    default:
-    printf("Error! Please, enter correct menu number.");
-    break;
-}
-
-return 0;
 }
 
 void DBLoader()
@@ -65,7 +74,21 @@ void DBSaver()
 
 void DBCreator()
 {
-
+    system("cls");
+    //const char *DBName;
+    printf("Hello, user. Let's create new DB.\n");
+    //printf("Enter new DB name: ");
+    //scanf("%c", &DBName);
+    FILE *fp;
+    fp=fopen("Mountains DB.txt", "a+t");
+    if(fp=NULL)
+    {
+        printf("Error DB creating\n");
+        exit(0);
+    }
+    system("cls");
+    printf("DB created.\n");
+    exit(1);
 }
 
 void addRecord()
