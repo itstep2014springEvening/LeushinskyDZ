8ba9ed4e33c5be5f942d715a1e54e8cf0afe1a8b
Somaxa <somaxa@mail.ru>
2014-11-12 13:00:46 +0300

    work.work

--- exam/main.c
+++ exam/main.c
@@ -2,57 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct _Mountain
+#define MAX 100
+
+
+struct Mountain
 {
     char mountainName [256];
     char mountainLocation [256];
     int mountainHeight;
     int mountainSlopeAngle;
     char mountainHasAGlacier [256];
-} Mountain;
+} Mountain_list[MAX];
 
+int find_free();
 void DBCreator();
-void addRecord(Mountain mountain);
-void DBLoader(Mountain mountain);
+void addRecord();
+void DBLoader();
 
+void init_mass_struct()
+{
+  int i;
+  for(i=0; i<MAX; ++i) Mountain_list[i].mountainName[0] = '\0';
+}
 
 
 int main()
 {
-    Mountain mountain;
-    int menuChooses;
-    do
+    int choice;
+    init_mass_struct();
+    for(;;)
     {
 
         printf("Hello, user. What do you want?\n\n");
         printf("1. Create new DB.\n");
-        printf("2. Add a new record.\n");
-        printf("3. Load DB.\n");
-        printf("4. Delete record.\n");
-        printf("5. Sort fields.\n");
+        printf("2. Add record.\n");
+        printf("3. Delete record.\n");
+        printf("4. Sort DB.\n");
+        printf("5. Save DB.\n");
+        printf("6. Load DB.\n");
         printf("0. Exit.\n");
-        scanf("%d", &menuChooses);
+        scanf("%d", &choice);
 
-
-
-
-        switch (menuChooses)
+        switch (choice)
         {
         case 1:
-            DBCreator();
+            createDB();
             break;
         case 2:
-        addRecord(mountain);
+            addRecord();
             break;
         case 3:
-    DBLoader(mountain);
+            deleteRecord();
             break;
         case 4:
-
+            sortDB();
             break;
         case 5:
-
+            saveDB();
             break;
+       /*  case 6:
+            loadDB();
+            break;*/
         case 0:
             system("cls");
             printf("Bye-bye, user.");
@@ -60,28 +70,67 @@ int main()
             break;
         default:
             system("cls");
-
             printf("Error! Please, enter correct menu number, or 0 to exit.\n\n");
             break;
         }
     }
-    while(menuChooses!=0);
     return 0;
 }
 
-void DBLoader(Mountain mountain)
+void createDB()
+{
+
+    system("cls");
+    //char DBName [256];
+    //printf("Hello, user. Let's create new DB.\n");
+    //printf("Enter new DB name: ");
+    //scanf("%s", &DBName);
+    FILE *fp;
+    fp=fopen("Mountains_DB.txt", "a+t");
+    if(fp=NULL)
+    {
+        printf("Error DB creating.\n");
+        exit(0);
+    }
+    system("cls");
+    printf("DB created.\n");
+    return;
+}
+
+void addRecord()
 {
     system("cls");
+    int slot;
+    slot=find_free();
+    if(slot==-1)
+    {
+        printf("DB is full");
+        return 0;
+    }
+
+    printf("Enter mountain name:\n");
+    scanf("%s", &Mountain_list[slot].mountainName);
+    printf("Enter mountain location:\n");
+    scanf("%s", &Mountain_list[slot].mountainLocation);
+    printf("Enter mountain height:\n");
+    scanf("%d", &Mountain_list[slot].mountainHeight);
+    printf("Enter mountain slope angle:\n");
+    scanf("%d", &Mountain_list[slot].mountainSlopeAngle);
+    printf("Mountain as a glacier(enter yes or no):\n");
+    scanf("%s", &Mountain_list[slot].mountainHasAGlacier);
+}
+
+/*
     char readFromFile [256];
     // printf("mountainName\n");
   // scanf("%s", &mountain.mountainName);
     FILE *fp;
     fp=fopen("Mountains_DB.txt", "a+t");
-    /*if(fp==NULL)
+    if(fp==NULL)
     {
         printf("Error");
         exit(1);
-    }*/
+    }
     int count;
     char i=0;
     int writedElements;
@@ -101,33 +150,65 @@ void DBLoader(Mountain mountain)
 
     fclose(fp);
     exit(1);
-}
+*/
 
-void DBSaver()
+void deleteRecord()
 {
-
-}
-
-void DBCreator()
-{
-    char DBName [256];
     system("cls");
-    //const char *DBName;
-    printf("Hello, user. Let's create new DB.\n");
-    printf("Enter new DB name: ");
-    scanf("%s", &DBName);
-    FILE *fp;
-    fp=fopen(DBName, "a+t");
-    if(fp=NULL)
+    int slot;
+    printf("Enter record N: ");
+    scanf("%d", &slot);
+    if(slot>=0 && slot<MAX)
     {
-        printf("Error DB creating.\n");
-        exit(0);
+        Mountain_list[slot].mountainName[0]='\0';
     }
-    system("cls");
-    printf("DB created.\n");
-    exit(1);
+    return;
 }
 
+void sortDB()
+{
+    system("cls");
+    for(int i=0; i<MAX; ++i)
+    {
+        if(Mountain_list[i].mountainName[0])
+        {
+            printf("Record N%d\n\n", i);
+            printf("Mountain name - %s\n", Mountain_list[i].mountainName);
+            printf("Mountain location - %s\n", Mountain_list[i].mountainLocation);
+            printf("Mountain height - %d\n", Mountain_list[i].mountainHeight);
+            printf("Mountain slope angle - %d\n", Mountain_list[i].mountainSlopeAngle);
+            printf("Mountain has a glacier - %s\n\n\n", Mountain_list[i].mountainHasAGlacier);
+        }
+    }
+    return;
+}
+
+void saveDB()
+{
+    FILE *fp;
+    char buffer [256];
+    fp=fopen("Mountain_DB.txt", "a+");
+    if(fp==NULL)
+    {
+        printf("Error DB opening.");
+        exit(1);
+    }
+    fwrite(Mountain_list[0].mountainName, sizeof buffer, 1, fp);
+    fclose(fp);
+    fp=fopen("Mountain_DB.txt", "a+");
+    if(fp==NULL)
+    {
+        printf("Error DB opening.");
+        exit(1);
+    }
+    fwrite(Mountain_list[0].mountainLocation, sizeof buffer, 1, fp);
+   // fwrite(Mountain_list[0].mountainHeight, sizeof buffer, 1, fp);
+    //fwrite(Mountain_list[0].mountainSlopeAngle, sizeof buffer, 1, fp);
+    //fwrite(Mountain_list[0].mountainHasAGlacier, sizeof buffer, 1, fp);
+
+}
+
+/*
 void addRecord(Mountain mountain)
 {
     char jsutNewStroke [256]="\n";
@@ -138,12 +219,12 @@ void addRecord(Mountain mountain)
    scanf("%s", &mountain.mountainName);
    // printf("mountainLocation\n");
    // scanf("%s", &mountain.mountainLocation);
-    /*printf("mountainHeight\n");
+    printf("mountainHeight\n");
     scanf("%d", &mountain.mountainHeight);
     printf("mountainSlopeAngle\n");
     scanf("%d", &mountain.mountainSlopeAngle);
     printf("mountainHasAGlacier\n");
-    scanf("%s", &mountain.mountainHasAGlacier);*/
+    scanf("%s", &mountain.mountainHasAGlacier);
     FILE *fp;
     fp=fopen("Mountains_DB.txt", "r+t");
     if(fp==NULL)
@@ -170,8 +251,16 @@ void DBSort()
 
 }
 
-
-
+*/
+int find_free()
+{
+  int freeSlot;
+  for(freeSlot=0; Mountain_list[freeSlot].mountainName[0] && freeSlot<MAX; ++freeSlot)
+  {
+       if(freeSlot==MAX) return -1;
+  }
+  return freeSlot;
+}
 
 
 
