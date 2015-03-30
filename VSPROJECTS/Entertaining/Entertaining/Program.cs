d197d989fc183927088d2ece702621f3cf6ca7a8
somaxa <somaxa@mail.ru>
2015-03-30 17:42:22 +0300

    up

--- VSPROJECTS/Entertaining/Entertaining/Program.cs
+++ VSPROJECTS/Entertaining/Entertaining/Program.cs
@@ -13,12 +13,18 @@ namespace Entertaining
     {
         static void Main(string[] args)
         {
-            string filepath = @"D:\git\LeushinskyDZ\VSPROJECTS\Entertaining\establishments.xml";
             XmlDocument xmld = new XmlDocument();
+            string DesiredTimeBegin = "";
+            string DesiredTimeEnd = "";
+            int NumberOfPeople = 0;
+            int MinimumAge = 0;
+           
+            string filepath = @"C:\vans\git\LeushinskyDZ\VSPROJECTS\Entertaining\establishments.xml";
+            
             FileStream fs = new FileStream(filepath, FileMode.Open);
             xmld.Load(fs);
             string typeofent = xmld.GetElementsByTagName("shebang")[1].FirstChild.Value;
-            string typeofent2 = xmld.GetElementsByTagName("shebang")[1].Attributes.Item(1).Value;
+            string typeofent2 = xmld.GetElementsByTagName("shebang")[1].Attributes.Item(4).Value;
 
 
             Console.WriteLine(typeofent2);
