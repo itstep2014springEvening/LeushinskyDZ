fba85f4f73ddb8c831cbf01adc9d27303ba53600
Somaxa <Somaxa@mail.ru>
2015-03-30 11:47:30 +0400

    va

---
+++ VSPROJECTS/Entertaining/Entertaining/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Entertaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"D:\git\LeushinskyDZ\VSPROJECTS\Entertaining\establishments.xml";
+            XmlDocument xmld = new XmlDocument();
+            FileStream fs = new FileStream(filepath, FileMode.Open);
+            xmld.Load(fs);
+            string typeofent = xmld.GetElementsByTagName("shebang")[1].FirstChild.Value;
+            string typeofent2 = xmld.GetElementsByTagName("shebang")[1].Attributes.Item(1).Value;
+
+
+            Console.WriteLine(typeofent2);
+        }
+    }
+}
