cea2dd42c6654dbd0751ce6a58a7aae1be32388e
Somaxa <Somaxa@mail.ru>
2015-04-01 16:35:59 +0400

    newMETA

--- VSPROJECTS/Entertaining/Entertaining/Program.cs
+++ VSPROJECTS/Entertaining/Entertaining/Program.cs
@@ -13,17 +13,11 @@ namespace Entertaining
 {
     internal class Program
     {
-        public class Establishments
-        {
-            public string TypeOfestablishment;
-            public string nameOfEstablishmentl;
-            public DateTime openTime;
-            public DateTime closeTime;
-            public string typeOfCompany;
-            public int ageLimit;
-        }
         private static void Main(string[] args)
         {
+            DateTime ztime;
+            string zcompany;
+            int zage;
             string filepath = @"D:\git\LeushinskyDZ\VSPROJECTS\Entertaining\establishments.xml";
            var organizations =  XDocument.
                 Load(new StreamReader(File.OpenRead(filepath)))
@@ -39,110 +33,28 @@ namespace Entertaining
                     element.Attribute("agelimit").Value
                     ))
                 .ToList();
-
-           organizations.ForEach(element =>
+             Console.WriteLine("Составьте запрос");
+            Console.WriteLine("Желаемое время: ");
+            ztime = DateTime.ParseExact(Console.ReadLine(), "HH:mm", null);
+            //Console.WriteLine("Сколько вас: ");
+          //  zcompany = Console.ReadLine();
+          //  Console.WriteLine("Ваш возраст: ");
+          //  zage = int.Parse(Console.ReadLine());
+            organizations.ForEach(e =>
+            {
+                if (e.OpenTime < ztime && ztime < e.CloseTime/*&&zage>e.AgeLimit&&zcompany*/)
                 {
-                    Console.WriteLine(element);
-                });
-            Console.ReadKey();
+                    Console.WriteLine(e.Type+e.Name);
+                }
+            });
+            //organizations.ForEach(element=>{Console.WriteLine("{0}",element.OpenTime<ztime);});
+           //organizations.ForEach(element =>
+           //     {
+           //         Console.WriteLine(element);
+           //     });
+           // Console.ReadKey();
             return ;
-            //clubNumber1.openTime = DateTime.ParseExact((xmld.GetElementsByTagName("bars")[0]
-            //    .ChildNodes[0].Attributes.Item(0).Value), "HH:mm", null);
-
-            //clubNumber1.closeTime = DateTime.ParseExact((xmld.GetElementsByTagName("bars")[0]
-            //    .ChildNodes[0].Attributes.Item(1).Value), "HH:mm", null);
-
-            //clubNumber1.typeOfCompany = xmld.GetElementsByTagName("bars")[0]
-            //    .ChildNodes[0].Attributes.Item(2).Value;
-
-            //clubNumber1.ageLimit = int.Parse((xmld.GetElementsByTagName("bars")[0]
-            //    .ChildNodes[0].Attributes.Item(3).Value));
-            //Console.WriteLine(clubNumber1.openTime);
-
-
-
-            //string DesiredTime = "";
-            //string NumberOfPeople = "";
-            //string MinimumAge = "";
-            //string[] openTime = new string[100];
-            //string[] closeTime = new string[100];
-            //string[] typeofpeople = new string[100];
-            //string[] agelimit = new string[100];
-            //string teststring = xmld.GetElementsByTagName("bars")[0].FirstChild.Attributes.Item(1).Value;
-            //Console.Write(teststring);
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    openTime[i] = xmld.GetElementsByTagName("bars")[0].FirstChild.Attributes.Item(i).Value;
-            //    Console.WriteLine(openTime[i]);
-            //}
-
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    openTime[i] = xmld.GetElementsByTagName("bars")[1].ChildNodes[i].Attributes.Item(i).Value;
-            //    Console.WriteLine(openTime[i]);
-            //}
-            //IEnumerable<Bar> bars;
-
-            //XDocument xmlDoc = XDocument.Load(new StreamReader(File.OpenRead(@"pathtofile")));
-
-            //xmlDoc
-            //    .Nodes()
-            //    .Select(n => new Bar()
-            //    {
-            //        Open = n.
-            //    })
-
-            //DateTime parsedOpendDate;
-            //bars.Where(b => b.Open >= parsedOpendDate && )
-            //    .ToList()
-            //    .ForEach(b => Console.WriteLine(b));
-
-            //foreach (var bar in bars.Where(b => b.Open >= parsedOpendDate && ).)
-            //{
-            //    Console.WriteLine(bar);
-            //}
-            /* Console.WriteLine("Здравствуйте, юзер. Куда пойдём?");
-            Console.WriteLine("Сформируйте запрос.");
-            Console.WriteLine("Введите время: ");
-            DesiredTime = Console.ReadLine();
-            Console.WriteLine("Сколько вас:");
-            NumberOfPeople = Console.ReadLine();
-            if(DesiredTime=="1")
-            {
-                DesiredTime = "one";
-            }
-            if(DesiredTime=="2")
-            {
-                DesiredTime = "pare";
-            }
-            //проверка на company
-            Console.WriteLine("Сколько лет вам:");
-            MinimumAge = Console.ReadLine();*/
-
-
-            // Console.WriteLine(xmld.GetElementsByTagName("bars")[0].FirstChild.InnerText); = печёный хряк
-            // Console.WriteLine(xmld.GetElementsByTagName("bars")[0].FirstChild.Attributes.Item(1).Value); = 02:00
-
-            // string teststring = xmld.GetElementsByTagName("bars")[0].FirstChild.Attributes.Item(0).Value;
-            // string teststring2 = xmld.GetElementsByTagName("bars")[0].FirstChild.Attributes.Item(1).Value;
-            //DateTime xxx = DateTime.ParseExact(teststring, "HH:mm", CultureInfo.InvariantCulture);
-            //DateTime xxx2 = DateTime.ParseExact(teststring2, "HH:mm", CultureInfo.InvariantCulture);
-            // if (xxx > xxx2)
-            // {
-            //     Console.WriteLine("Y");
-            // }
-            // else
-            // {
-            //     Console.WriteLine("N");
-            // }
-            // Console.WriteLine(teststring);
-
-
-            // NumberOfPeople = (int)xmld.GetElementsByTagName("shebang")[1].Attributes.Item(4).Value;
-            // NumberOfPeople = Convert.ToInt32(10);
-
-
-            // Console.WriteLine(typeofent);
+           
         }
     }
 
@@ -151,7 +63,7 @@ namespace Entertaining
         public string Type { get; private set; }
         public string Name { get; private set; }
         public DateTime OpenTime { get; set; }
-        public string CloseTime { get; set; }
+        public DateTime CloseTime { get; set; }
         public string TypeOfPeople { get; set; }
         public string AgeLimit { get; set; }
 
@@ -168,10 +80,15 @@ namespace Entertaining
                 throw new ArgumentException("openTime");
             }
 
+            DateTime closeTimeAsDateTime;
+            if (DateTime.TryParse(closeTime, out closeTimeAsDateTime) == false)
+            {
+                throw new ArgumentException("openTime");
+            }
             Type = type;
             Name = name;
             OpenTime = openTimeAsDateTime;
-            CloseTime = closeTime;
+            CloseTime = closeTimeAsDateTime;
             TypeOfPeople = typeOfPeople;
             AgeLimit = ageLimit;
             
@@ -189,14 +106,3 @@ namespace Entertaining
         }
     }
 }
-
-//    class Bar
-//    {
-//        public DateTime Open { get; set; }
-
-//        public void SetOpen(string stringDate)
-//        {
-
-//        }
-//    }
-//}
