f31e812ce64e01bf2094ee46312611e2c38bd8ff
Somaxa <Somaxa@mail.ru>
2015-04-08 17:36:18 +0400

    dz2

--- winforms/dz2part2/Form1.cs
+++ winforms/dz2part2/Form1.cs
@@ -19,15 +19,9 @@ namespace dz2part2
            // MouseHover += button1_MouseHover;
         }
 
-        private void button1_MouseHover(object sender, EventArgs e)
-        {
-            button1.Location = new Point(rand.Next(50, 1000), rand.Next(50, 1000));
-        }
+       
 
-        void Form1_MouseMove(object sender, MouseEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+       
 
         private void Form1_Load(object sender, EventArgs e)
         {
