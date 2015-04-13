2d3818fa4c590a5a9f7867d15fdc5d26e114cb7a
somaxa <somaxa@mail.ru>
2015-04-13 21:15:34 +0300

    dz5

---
+++ winforms/dz5/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace dz5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 383);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Let\'s GO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(124, 383);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 45);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Restart";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(867, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(850, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(64, 291);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(124, 123);
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(760, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(124, 235);
+            this.progressBar2.MarqueeAnimationSpeed = 1;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(760, 23);
+            this.progressBar2.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar2.TabIndex = 8;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(669, 346);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(175, 45);
+            this.trackBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 440);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Race";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ winforms/dz5/Form1.cs
@@ -0,0 +1,100 @@
+﻿//http://habrahabr.ru/post/213809/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dz5
+{
+    
+    public partial class Form1 : Form
+    {
+        List<Car> Cars = new List<Car>();
+        Timer RaceTimer = new Timer();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 10;
+            RaceTimer.Tick += RaceTimer_Tick;
+            RaceTimer.Interval = 1;
+            
+            label1.Text = "F\nI\nN\nI\nS\nH";
+            
+            
+            car1.CarFinished += car1_CarFinished;
+            car2.CarFinished += car2_CarFinished;
+            car1.Location = new Point(124,152);
+            car1.Size = new Size(75,23);
+            car1.Text = "BMW";
+            car1.BringToFront();
+            this.Controls.Add(car1);
+            car2.Location = new Point(124, 206);
+            car2.Size = new Size(75, 23);
+            car2.Text = "LADA";
+            car2.BringToFront();
+            this.Controls.Add(car2);
+
+        }
+
+        void car2_CarFinished()
+        {
+            
+        }
+
+        void car1_CarFinished()
+        {
+            MessageBox.Show("AHAHA OLOLO");
+       
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            RaceTimer.Start();
+        
+            //button1.Left += 7;
+        }
+
+        void RaceTimer_Tick(object sender, EventArgs e)
+        {
+            
+            Random r = new Random();
+            car1.Left+=r.Next(1,5)+trackBar1.Value;
+            car2.Left += r.Next(1, 6) + trackBar1.Value;
+            foreach(var car in Cars)
+            {
+                if ((car.Left + car.Width) >= groupBox1.Left)
+                {
+                    RaceTimer.Stop();
+                    car.CarFinish();
+                }
+            }
+            
+                
+               
+        }
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RaceTimer.Stop();
+            car1.Left = 124;
+            car2.Left = 124;
+            
+        }
+    }
+}
