3b009622443dd6be742ff1ee2b2c4b465196cc7f
Somaxa <Somaxa@mail.ru>
2015-04-13 12:48:29 +0400

    dz

--- winforms/dz2part2/Form1.Designer.cs
+++ winforms/dz2part2/Form1.Designer.cs
@@ -28,21 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Press me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 532);
+            this.ClientSize = new System.Drawing.Size(734, 712);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Press the button";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
 
     }
 }
--- winforms/dz2part2/Form1.cs
+++ winforms/dz2part2/Form1.cs
@@ -12,11 +12,10 @@ namespace dz2part2
 {
     public partial class Form1 : Form
     {
-        Random rand = new Random();
+        
         public Form1()
         {
             InitializeComponent();
-           // MouseHover += button1_MouseHover;
         }
 
        
@@ -26,6 +25,27 @@ namespace dz2part2
         private void Form1_Load(object sender, EventArgs e)
         {
             
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //int button1X = int.Parse(button1.Location.X.ToString());
+            //int button1Y = int.Parse(button1.Location.Y.ToString());
+            //if (e.X == button1X)
+            //    MessageBox.Show("LALA");
+
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+            button1.Left = r.Next(0, this.ClientSize.Width - button1.Width);
+            button1.Top = r.Next(0, this.ClientSize.Height - button1.Height);
         }
     }
 }
