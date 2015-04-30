923968497c0dd67921f12f0263cfb364b62302b8
Somaxa <Somaxa@mail.ru>
2015-04-30 17:27:29 +0400

    fdz

--- winforms/dz8/Form1.Designer.cs
+++ winforms/dz8/Form1.Designer.cs
@@ -29,25 +29,25 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Мама");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Папа");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Близкие родственники", new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2});
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Дальние родственники");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Родственники", new System.Windows.Forms.TreeNode[] {
-            treeNode3,
-            treeNode4});
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Фирмы");
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Отдела");
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Сотрудники", new System.Windows.Forms.TreeNode[] {
-            treeNode6,
-            treeNode7});
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Друзья");
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Получатели", new System.Windows.Forms.TreeNode[] {
-            treeNode5,
-            treeNode8,
-            treeNode9});
+            System.Windows.Forms.TreeNode treeNode31 = new System.Windows.Forms.TreeNode("Мама");
+            System.Windows.Forms.TreeNode treeNode32 = new System.Windows.Forms.TreeNode("Папа");
+            System.Windows.Forms.TreeNode treeNode33 = new System.Windows.Forms.TreeNode("Близкие родственники", new System.Windows.Forms.TreeNode[] {
+            treeNode31,
+            treeNode32});
+            System.Windows.Forms.TreeNode treeNode34 = new System.Windows.Forms.TreeNode("Дальние родственники");
+            System.Windows.Forms.TreeNode treeNode35 = new System.Windows.Forms.TreeNode("Родственники", new System.Windows.Forms.TreeNode[] {
+            treeNode33,
+            treeNode34});
+            System.Windows.Forms.TreeNode treeNode36 = new System.Windows.Forms.TreeNode("Фирмы");
+            System.Windows.Forms.TreeNode treeNode37 = new System.Windows.Forms.TreeNode("Отдела");
+            System.Windows.Forms.TreeNode treeNode38 = new System.Windows.Forms.TreeNode("Сотрудники", new System.Windows.Forms.TreeNode[] {
+            treeNode36,
+            treeNode37});
+            System.Windows.Forms.TreeNode treeNode39 = new System.Windows.Forms.TreeNode("Друзья");
+            System.Windows.Forms.TreeNode treeNode40 = new System.Windows.Forms.TreeNode("Получатели", new System.Windows.Forms.TreeNode[] {
+            treeNode35,
+            treeNode38,
+            treeNode39});
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.treeView1 = new System.Windows.Forms.TreeView();
@@ -63,12 +63,28 @@
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -79,6 +95,8 @@
             this.splitContainer2.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -94,8 +112,8 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(758, 518);
-            this.splitContainer1.SplitterDistance = 222;
+            this.splitContainer1.Size = new System.Drawing.Size(731, 514);
+            this.splitContainer1.SplitterDistance = 214;
             this.splitContainer1.TabIndex = 0;
             // 
             // treeView1
@@ -104,30 +122,30 @@
             this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
-            treeNode1.Name = "Node0";
-            treeNode1.Text = "Мама";
-            treeNode2.Name = "Node1";
-            treeNode2.Text = "Папа";
-            treeNode3.Name = "Node7";
-            treeNode3.Text = "Близкие родственники";
-            treeNode4.Name = "Node8";
-            treeNode4.Text = "Дальние родственники";
-            treeNode5.Name = "Node1";
-            treeNode5.Text = "Родственники";
-            treeNode6.Name = "Node9";
-            treeNode6.Text = "Фирмы";
-            treeNode7.Name = "Node10";
-            treeNode7.Text = "Отдела";
-            treeNode8.ImageIndex = -2;
-            treeNode8.Name = "Node4";
-            treeNode8.Text = "Сотрудники";
-            treeNode9.Name = "Node6";
-            treeNode9.Text = "Друзья";
-            treeNode10.Name = "Node0";
-            treeNode10.Text = "Получатели";
+            treeNode31.Name = "Node0";
+            treeNode31.Text = "Мама";
+            treeNode32.Name = "Node1";
+            treeNode32.Text = "Папа";
+            treeNode33.Name = "Node7";
+            treeNode33.Text = "Близкие родственники";
+            treeNode34.Name = "Node8";
+            treeNode34.Text = "Дальние родственники";
+            treeNode35.Name = "Node1";
+            treeNode35.Text = "Родственники";
+            treeNode36.Name = "Node9";
+            treeNode36.Text = "Фирмы";
+            treeNode37.Name = "Node10";
+            treeNode37.Text = "Отдела";
+            treeNode38.ImageIndex = -2;
+            treeNode38.Name = "Node4";
+            treeNode38.Text = "Сотрудники";
+            treeNode39.Name = "Node6";
+            treeNode39.Text = "Друзья";
+            treeNode40.Name = "Node0";
+            treeNode40.Text = "Получатели";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode10});
-            this.treeView1.Size = new System.Drawing.Size(222, 518);
+            treeNode40});
+            this.treeView1.Size = new System.Drawing.Size(214, 514);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
             // 
@@ -144,13 +162,14 @@
             // 
             // splitContainer2.Panel2
             // 
+            this.splitContainer2.Panel2.Controls.Add(this.label6);
             this.splitContainer2.Panel2.Controls.Add(this.button2);
             this.splitContainer2.Panel2.Controls.Add(this.button1);
             this.splitContainer2.Panel2.Controls.Add(this.label5);
             this.splitContainer2.Panel2.Controls.Add(this.label4);
             this.splitContainer2.Panel2.Controls.Add(this.textBox2);
-            this.splitContainer2.Size = new System.Drawing.Size(532, 518);
-            this.splitContainer2.SplitterDistance = 212;
+            this.splitContainer2.Size = new System.Drawing.Size(513, 514);
+            this.splitContainer2.SplitterDistance = 242;
             this.splitContainer2.TabIndex = 0;
             // 
             // tabControl1
@@ -162,7 +181,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(532, 212);
+            this.tabControl1.Size = new System.Drawing.Size(513, 242);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -177,7 +196,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(524, 186);
+            this.tabPage1.Size = new System.Drawing.Size(505, 216);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Главное";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -251,13 +270,24 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.groupBox1);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(524, 186);
+            this.tabPage3.Size = new System.Drawing.Size(524, 218);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Настройки";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(295, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
             // 
             // button2
             // 
@@ -277,6 +307,7 @@
             this.button1.TabIndex = 10;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label5
             // 
@@ -309,14 +340,145 @@
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             this.imageList1.Images.SetKeyName(0, "frnds.png");
             // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 1500;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(12, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(504, 209);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки SMTP";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Сервер";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Порт";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 121);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Пароль";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 42);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 13);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Адрес";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 193);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(85, 13);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "Включить SSL?";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(6, 95);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 13);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "Аккаунт";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(89, 13);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(89, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(89, 92);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(121, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(89, 118);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(121, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(97, 192);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(758, 518);
+            this.ClientSize = new System.Drawing.Size(731, 514);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Мессенджер";
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -329,6 +491,9 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -355,6 +520,22 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
 
     }
 }
--- winforms/dz8/Form1.cs
+++ winforms/dz8/Form1.cs
@@ -7,19 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
 
 namespace dz8
 {
     public partial class Form1 : Form
     {
+        private List<MailEvents> _events;
+        Timer MailTimer = new Timer();
+        SmtpClient smtp;
+        List<TreeNode> ListOfChekedNodes = new List<TreeNode>();
+        Dictionary<TreeNode, string> NameAndEmails = new Dictionary<TreeNode, string>();
+        string[] eMails = { "dardillov999@mail.ru", "dardillov999@mail.ru", "dardillov999@mail.ru", "dardillov999@mail.ru", "dardillov999@mail.ru",
+                          "dardillov999@mail.ru","dardillov999@mail.ru","dardillov999@mail.ru","dardillov999@mail.ru","dardillov999@mail.ru"};
+        MailMessage message = new MailMessage();
+        string[] Servers = { "MAIL.RU" };
+        
         //string[] nodeNames = new string[100];
         public Form1()
         {
+            _events = new List<MailEvents>();
             InitializeComponent();
+            MailTimer.Interval = 1000;
+            MailTimer.Start();
+            MailTimer.Tick += MailTimer_Tick;
+            smtp = new SmtpClient("smtp.mail.ru", 25);
+            smtp.Credentials = new NetworkCredential("228overlord228@mail.ru", "overlordoverlordoverlordoverlord");
+            smtp.EnableSsl = true;
+            string[] Occasions = { "День рождения", "Свадьба","Проводы","Крестины","Годовщина" };
+            comboBox1.DataSource = Occasions;
+            comboBox2.DataSource = Servers;
+            NameAndEmails.Add(treeView1.Nodes[0], eMails[0]);
+            backgroundWorker1.DoWork+=backgroundWorker1_DoWork;
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+
             
             
         }
-        List<TreeNode> ListOfChekedNodes = new List<TreeNode> ();
+
+        void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            toolTip1.Show("Письмо отправлено", label6);
+        }
+
+        void MailTimer_Tick(object sender, EventArgs e)
+        {
+            foreach(MailEvents me in _events)
+            {
+                if (me.EventTime.Second == DateTime.Now.Second && me.EventTime.Minute == DateTime.Now.Minute)
+                {
+
+                    backgroundWorker1.RunWorkerAsync(me);
+                  
+                        
+                    
+                }
+            }
+        }
+        
+       
+        
         string CheckedNodes = "";
         public List<TreeNode> GetNameNodes (TreeNode tr)
         {
@@ -40,16 +89,44 @@ namespace dz8
             }
             return ListOfChekedNodes;
         }
+        public void CheckChildrensIfParentsChecked(TreeNode tr)
+        {
+            for(int i=0;i<tr.Nodes.Count;i++)
+            {
+                tr.Nodes[i].Checked = tr.Checked;
+                if(tr.Nodes==null)
+                {
+                    NameAndEmails.Add(tr.Nodes[i], eMails[i]);
+                }
+                
+                if (tr.Nodes[i].Checked)
+                    tr.ExpandAll();
+                //if(tr.Checked&&tr.Nodes.Count!=0)
+                //{
+                //    tr.Nodes[i].Checked=true;
+                //}
+                CheckChildrensIfParentsChecked(tr.Nodes[i]);
+            }
+        }
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox2.Text = "";
-            TreeNode tr = treeView1.TopNode;
+            DateTime DateTimeOnPicker = new DateTime(monthCalendar1.SelectionStart.Year, monthCalendar1.SelectionStart.Month, monthCalendar1.SelectionStart.Day, dateTimePicker1.Value.Hour, dateTimePicker1.Value.Minute,dateTimePicker1.Value.Second);
             
-            GetNameNodes(tr);
-            foreach (TreeNode trnd in ListOfChekedNodes)
-            {
-                textBox2.Text += trnd.Text;
-            }
+            MailEvents me = new MailEvents();
+            me.Message = textBox2.Text;
+            if (comboBox1.SelectedValue != null)
+            me.Occasion = comboBox1.SelectedValue.ToString();
+            me.Recipient = NameAndEmails[treeView1.Nodes[0]];
+            me.EventDate = monthCalendar1.SelectionStart;
+            me.EventTime = DateTimeOnPicker;
+
+            
+            //GetNameNodes(tr);
+            
+            //foreach (TreeNode trnd in ListOfChekedNodes)
+            //{
+            //    textBox2.Text += trnd.Text;
+            //}
             
 
             //for(int i=0;i<nodeNames.Length;i++)
@@ -76,11 +153,43 @@ namespace dz8
             //    }
             //}
                 //MessageBox.Show(i.ToString());
+            _events.Add(me);
         }
 
         private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
         {
+            CheckChildrensIfParentsChecked(e.Node);
+        }
+
+        
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var me = (MailEvents)e.Argument;
+            message.From = new MailAddress("228overlord228@mail.ru");
+            message.To.Add(new MailAddress(me.Recipient));
+            message.Subject = me.Occasion;
+            message.Body = me.Message;
+            smtp.Send(message);
             
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.ShowDialog();
+            string file = opf.FileName;
+            Attachment attach = new Attachment(file, MediaTypeNames.Application.Pdf);
+
+            // Добавляем информацию для файла
+            ContentDisposition disposition = attach.ContentDisposition;
+            disposition.CreationDate = System.IO.File.GetCreationTime(file);
+            disposition.ModificationDate = System.IO.File.GetLastWriteTime(file);
+            disposition.ReadDate = System.IO.File.GetLastAccessTime(file);
+            //добавление вложения
+            message.Attachments.Add(attach);
+
         }
     }
 }
