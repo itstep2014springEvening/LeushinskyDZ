8d5d71d7d3891ce7043e67417d861b3f41979136
somaxa <somaxa@mail.ru>
2015-09-19 12:26:31 +0300

    ado

---
+++ ado/ExamplesDisConnected/Example1DataTable/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example1DataTable
+{
+    public partial class MainForm : Form
+    {
+        DataSet dataSet;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                // Приязка к таблицам в качестве источника BindingSource
+                dgwDrivers.DataSource = bsDrivers;
+                dgwCars.DataSource = bsCars;
+
+                // BindingSource - предназначен для упрощения процесса 
+                // привязки элементов управления к источнику данных
+
+
+                // Создание DataSet с именем AutoPark
+                dataSet = new DataSet("AutoPark");
+                // Формирование заполнение dataSet  из  XML-файла
+                dataSet.ReadXml("Registration.xml");
+                dataSet.ReadXmlSchema("Registration.xsd");
+
+                // присваивание dataSet объекту BindingSource
+                bsDrivers.DataSource = dataSet;
+                // указываем таблицу для свойства DataMember
+                bsDrivers.DataMember = "Drivers";
+
+           
+                dgwCars.DataSource = bsDrivers;
+                dgwCars.DataMember = "FK_DriversCars";
+
+            }
+
+    }
+}
---
+++ ado/ExamplesDisConnected/Example7CommandBuilder/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example7CommandBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=A1-PREPOD;Initial Catalog=ShopDB;Integrated Security=True;";
+            string commandString = "SELECT * FROM Customers";
+
+            DataTable customers = new DataTable("Customers");
+
+            SqlDataAdapter adapter = new SqlDataAdapter(commandString, connectionString);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+           
+            adapter.Fill(customers);
+
+            //*********************************************Adding Data******************************************
+
+            customers.LoadDataRow(new object[] {100,"TEST", "TEST", "TEST", "TEST", "9999/12/31" }, false);
+
+            adapter.Update(customers);
+
+            customers.Clear();
+            adapter.Fill(customers);
+
+            Console.WriteLine("Customers after adding test rows");
+
+            foreach (DataRow row in customers.Rows)
+            {
+                foreach (DataColumn column in customers.Columns)
+                    Console.WriteLine("{0}: {1}", column.ColumnName, row[column]);
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+
+            //*********************************************Changing Data******************************************
+
+            DataRow[] changedRows = customers.Select("Phone = 'TEST'");
+
+            foreach (DataRow singleRow in changedRows)
+            {
+                singleRow[1] = "ChangedValue";
+                singleRow[2] = "ChangedValue";
+                singleRow[3] = "ChangedValue";
+            }
+
+            adapter.Update(customers);
+
+            customers.Clear();
+            adapter.Fill(customers);
+
+            Console.WriteLine("Customers after changing test rows");
+
+            foreach (DataRow row in customers.Rows)
+            {
+                foreach (DataColumn column in customers.Columns)
+                    Console.WriteLine("{0}: {1}", column.ColumnName, row[column]);
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+
+            //*********************************************Deleting Data******************************************
+            DataRow[] rowsToDelete = customers.Select("Phone = 'TEST'");
+
+            foreach (DataRow singleRow in rowsToDelete)
+            {
+                singleRow.Delete();
+            }
+
+            adapter.Update(customers);
+
+            customers.Clear();
+            adapter.Fill(customers);
+
+            Console.WriteLine("Customers after deleting test rows");
+
+            foreach (DataRow row in customers.Rows)
+            {
+                foreach (DataColumn column in customers.Columns)
+                    Console.WriteLine("{0}: {1}", column.ColumnName, row[column]);
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+
+    }
+}
